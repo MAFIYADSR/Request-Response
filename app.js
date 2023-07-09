a2499f700e408fdc2da86f4f13afd1d033b1b6b4
Akash  Dutta <104191461+MAFIYADSR@users.noreply.github.com>
2023-07-09 13:43:01 +0530

    response.js

--- app.js
+++ app.js
@@ -1,22 +1,35 @@
-// const http = require('http');
 
-// const server = http.createServer((req, res) =>
-// {
-//     res.end("Akash dutta");
-// })
-
-// server.listen(4000)
-
+// const {console} =require('console');
 const http = require('http');
 const port = 4000;
+
 const server = http.createServer((req, res) => {
-    // console.log(req.url);
-    
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html')
-    res.end('<h1> Akash dutta </h1>')
+    console.log(req.url);
+
+    if(req.url == '/')
+    {
+        res.end('<h1> Akash dutta </h1>')
+    }
+    else if(req.url == '/home'){
+        res.end('<h1> Welcome home </ht>');
+    }
+    else if(req.url == '/about')
+    {
+        res.end('<h1>Welcome to About Us page</h1>')
+    }
+    else if(req.url == '/node'){
+        res.end('<h1>Welcome to my Node Js project</h1>')
+    }
+    
+    
 })
 
 server.listen(port, ()=>{
     console.log(`Server is listining on port ${port}`);
 })
+
+
+
+
